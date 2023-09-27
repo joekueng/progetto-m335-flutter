58e013a709ae0dc93ffc7a56e171c1ca1f1f8cf7
Joe Küng <joekueng05@gmail.com>
2023-09-27 13:43:04 +0200

    database

---
+++ lib/database/database.dart
@@ -0,0 +1,36 @@
+
+import 'package:progetto_m335_flutter/model/note.dart';
+
+class Database{
+  static final Database _instance = Database._init();
+  static Database? _database;
+
+  Database._init();
+
+  Future<Database> get database async {
+    if (_database != null) return _database!;
+    _database = await _initDB('database.db');
+    return _database!;
+  }
+
+  Future _createDB(Database database) async{
+    const integerPrimaryKeyAutoincrement = 'INTEGER PRIMARY KEY AUTOINCREMENT';
+    const textNotNull = 'TEXT NOT NULL';
+    const integerNotNull = 'INTEGER NOT NULL';
+    const integer = 'INTEGER';
+    const real = 'REAL';
+    const text = 'TEXT';
+
+
+    await database.execute('''
+      CREATE TABLE $Note (
+        ${Note.id} $integerPrimaryKeyAutoincrement,
+        
+      )
+    ''');
+  }
+
+  execute(String s) {
+
+  }
+}
