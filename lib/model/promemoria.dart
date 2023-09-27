300062b2d20862ef7cc9b472dc83018fec9db8cb
Joe Küng <joekueng05@gmail.com>
2023-09-27 13:50:56 +0200

    new directory

--- lib/model/promemoria.dart
+++ lib/model/promemoria.dart
@@ -1,19 +1,18 @@
+import 'dart:ui';
+
 import 'base_entity.dart';
 import 'identifiers/enum/color.dart';
 import 'identifiers/enum/priority.dart';
 
-const String promemoriaTable = 'promemoria';
+class Note extends BaseEntity{
+  static String id = BaseEntity.getId;
+  static String title = BaseEntity.getTitle;
+  static String creationDate = BaseEntity.getCreationDate;
 
-class Promemoria extends BaseEntity {
-  static String expirationDate = '';
-  static String arrayPromemoria = '';
-  String description = '';
-  static Priority priority = Priority.none;
-
-  static Color color = Color.none;
-
-  Promemoria(String description){
-    this.description = description;
-  }
+  static String expirationDate = 'expirationDate';
+  static String description = 'description';
+  Priority priority = Priority.none;
+  Color color = Color.none;
 
 }
+
