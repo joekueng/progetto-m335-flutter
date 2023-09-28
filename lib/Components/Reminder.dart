29a54bbcbcbeb023e2f8216fbc95bbd1e9dcd774
Tito Arrigo <titoarrigo@gmail.com>
2023-09-28 09:49:09 +0200

    grafica pagina TodayView.dart

---
+++ lib/Components/Reminder.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+import '../Components/EditReminder.dart';
+
+class Reminder extends StatefulWidget {
+  const Reminder({super.key});
+
+  @override
+  State<Reminder> createState() => _ReminderState();
+}
+
+class _ReminderState extends State<Reminder> {
+  bool _value = false;
+
+  void _onChanged(bool? newValue) {
+    setState(() {
+      _value = newValue ?? false;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: Checkbox(
+        value: _value,
+        onChanged: _onChanged,
+      ),
+      title: Text("Reminder"),
+      subtitle: Text(DateTime.now().toString()),
+      trailing: EditReminder(),
+    );
+  }
+}
