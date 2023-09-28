a58721a6f4bd8abf626f3790743429da3d2f21b9
grata <aramis.grata@bluewin.ch>
2023-09-28 13:57:21 +0200

    crud

--- lib/model/note.dart
+++ lib/model/note.dart
@@ -9,4 +9,63 @@ class Note extends BaseEntity {
   static String lastModificationDate = BaseEntity.getLastEditDate;
   static String arrayPromemoria = '';
   static String description = '';
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'title': title,
+      'creationDate': creationDate,
+      'lastModificationDate': lastModificationDate,
+      'arrayPromemoria': arrayPromemoria,
+      'description': description
+    };
+  }
+
+  String getId() {
+    return id;
+  }
+
+  void setId(String id) {
+    id = id;
+  }
+
+  String getTitle() {
+    return title;
+  }
+
+  void setTitle(String title) {
+    title = title;
+  }
+
+  String getCreationDate() {
+    return creationDate;
+  }
+
+  void setCreationDate(String creationDate) {
+    creationDate = creationDate;
+  }
+
+  String getLastModificationDate() {
+    return lastModificationDate;
+  }
+
+  void setLastModificationDate(String lastModificationDate) {
+    lastModificationDate = lastModificationDate;
+  }
+
+  String getArrayPromemoria() {
+    return arrayPromemoria;
+  }
+
+  void setArrayPromemoria(String arrayPromemoria) {
+    arrayPromemoria = arrayPromemoria;
+  }
+
+  String getDescription() {
+    return description;
+  }
+
+  void setDescription(String description) {
+    description = description;
+  }
 }
