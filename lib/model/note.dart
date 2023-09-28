9b3634e1cdd21681eb39160f570f288994466eed
Joe Küng <joekueng05@gmail.com>
2023-09-28 08:47:50 +0200

    entity

--- lib/model/note.dart
+++ lib/model/note.dart
@@ -1,9 +1,11 @@
 import 'base_entity.dart';
 
+const String noteTable = 'note';
+
 class Note extends BaseEntity {
   static String id = BaseEntity.getId;
   static String title = BaseEntity.getTitle;
   static String creationDate = BaseEntity.getCreationDate;
-  static String lastModificationDate = "LastModificationDate";
-  static String description = "";
+  static String lastModificationDate = BaseEntity.getLastEditDate;
+  static String description = '';
 }
