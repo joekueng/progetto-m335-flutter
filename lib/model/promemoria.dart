c2824c99a42016ca9ae0720c5ffc9a849c2a2e86
Tito Arrigo <titoarrigo@gmail.com>
2023-09-28 13:04:42 +0200

    merge

---
+++ lib/model/promemoria.dart
@@ -0,0 +1,18 @@
+import 'base_entity.dart';
+import 'identifiers/enum/color.dart';
+import 'identifiers/enum/priority.dart';
+
+const String promemoriaTable = 'promemoria';
+
+class Promemoria extends BaseEntity {
+  static String id = BaseEntity.getId;
+  static String title = BaseEntity.getTitle;
+  static String creationDate = BaseEntity.getCreationDate;
+  static String lastModificationDate = BaseEntity.getLastEditDate;
+  static String expirationDate = '';
+  static String arrayPromemoria = '';
+  static String description = '';
+  static Priority priority = Priority.none;
+
+  static Color color = Color.none;
+}
