98c04bfc165be431b53d355bf0a3597f67d3e64e
grata <aramis.grata@bluewin.ch>
2023-09-29 14:26:09 +0200

    removed controller

--- lib/database/database.dart
+++ lib/database/database.dart
@@ -1,14 +1,13 @@
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
+import '../model/note.dart';
 import 'FireDb.dart';
 import 'package:progetto_m335_flutter/model/promemoria.dart';
-import 'controller.dart';
 
 class NoteDatabase {
   static final NoteDatabase instance = NoteDatabase._init();
   static Database? _database;
   FireDb fireDb = FireDb();
-  Controller controller = Controller();
 
   NoteDatabase._init();
 
@@ -51,7 +50,7 @@ class NoteDatabase {
     ''');
 
     print("database created");
-    controller.getDataFromFirebase(database, version);
+    getDataFromFirebase(database, version);
   }
 
   Future<List<Map>> selectAllPromemoria() async {
@@ -60,5 +59,110 @@ class NoteDatabase {
     final List<Map<String, dynamic>> maps = await db.query(promemoriaTable);
 
     return maps;
+  }
+
+  Future<List<Note>> getAllNote() async {
+    var notes = await _database?.query(noteTable);
+    List<Note> noteList = notes!.map((e) => Note.fromJson(e)).toList();
+    return noteList;
+  }
+
+  Future<List<Promemoria>> getAllPromemoria() async {
+    var promemorias = await _database?.query(promemoriaTable);
+    List<Promemoria> promemoriaList =
+    promemorias!.map((e) => Promemoria.fromJson(e)).toList();
+    return promemoriaList;
+  }
+
+  Future<Note> getNoteById(int id) async {
+    var note = await _database?.query(noteTable, where: 'id = ?', whereArgs: [id]);
+    return Note.fromJson(note!.first);
+  }
+
+  Future<Promemoria> getPromemoriaById(int id) async {
+    var promemoria =
+    await _database?.query(promemoriaTable, where: 'id = ?', whereArgs: [id]);
+    return Promemoria.fromJson(promemoria!.first);
+  }
+
+  //add note
+  void addNote(Note note) async {
+    await _database?.execute('''
+  INSERT INTO note (
+    title,
+    creationDate,
+    lastModificationDate,
+    arrayPromemoria,
+    description
+  ) VALUES (
+    '${note.title}',
+    '${note.creationDate}',
+    '${note.lastModificationDate}',
+    '${note.arrayPromemoria}',
+    '${note.description}'
+  )
+''');
+  }
+
+  //add Promemoria
+  void addPromemoria(Promemoria promemoria) async {
+    await _database?.execute('''
+     INSERT INTO promemoria (
+        title,
+        creationDate,
+        lastModificationDate,
+        expirationDate,
+        arrayPromemoria,
+        description,
+        priority,
+        color
+      ) VALUES (
+        '${promemoria.title}',
+        '${promemoria.creationDate}',
+        '${promemoria.lastModificationDate}',
+        '${promemoria.expirationDate}',
+        '${promemoria.arrayPromemoria}',
+        '${promemoria.description}',
+        '${promemoria.priority}',
+        '${promemoria.color}'
+      )
+    ''');
+  }
+
+  void deleteAll() async {
+    await _database?.execute('''
+      DELETE FROM promemoria
+    ''');
+
+    await _database?.execute('''
+      DELETE FROM note
+    ''');
+  }
+
+
+  void getDataFromFirebase(Database database, int version) async {
+    this.deleteAll();
+
+    var promemorias = await fireDb.readAllPromemoria();
+    var notes = await fireDb.readAllNotes();
+
+    for (var promemoria in promemorias) {
+      this.addPromemoria(promemoria);
+    }
+
+    for (var note in notes) {
+      this.addNote(note);
+    }
+  }
+
+  void syncData() async {
+    var promemorias = await getAllPromemoria();
+    var notes = await getAllNote();
+
+    await fireDb.deleteAllPromemoria();
+    await fireDb.deleteAllNotes();
+
+    await fireDb.createAllPromemoria(promemorias);
+    await fireDb.createAllNotes(notes);
   }
 }
