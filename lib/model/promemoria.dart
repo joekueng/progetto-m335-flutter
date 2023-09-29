cd1f6238f8d36bce6e94db8dc29398f642375a6b
grata <aramis.grata@bluewin.ch>
2023-09-29 09:40:35 +0200

    Added Firebase

--- lib/model/promemoria.dart
+++ lib/model/promemoria.dart
@@ -4,16 +4,37 @@ import 'identifiers/enum/priority.dart';
 
 const String promemoriaTable = 'promemoria';
 
-class Promemoria extends BaseEntity {
-  static String id = BaseEntity.getId;
-  static String title = BaseEntity.getTitle;
-  static String creationDate = BaseEntity.getCreationDate;
-  static String lastModificationDate = BaseEntity.getLastEditDate;
-  static String expirationDate = '';
-  static String arrayPromemoria = '';
-  static String description = '';
-  static Priority priority = Priority.none;
-  static Color color = Color.none;
+class Promemoria {
+  String id = '';
+  String title;
+  String creationDate;
+  String lastModificationDate;
+  String expirationDate;
+  String arrayPromemoria;
+  String description;
+  String priority;
+  String color;
+
+  Promemoria(
+      this.id,
+      this.title,
+      this.creationDate,
+      this.lastModificationDate,
+      this.expirationDate,
+      this.arrayPromemoria,
+      this.description,
+      this.priority,
+      this.color);
+
+  Promemoria.newConstructor(
+      this.title,
+      this.creationDate,
+      this.lastModificationDate,
+      this.expirationDate,
+      this.arrayPromemoria,
+      this.description,
+      this.priority,
+      this.color);
 
   Map<String, dynamic> toMap() {
     return {
@@ -29,69 +50,92 @@ class Promemoria extends BaseEntity {
     };
   }
 
-  String getId(){
+  String getId() {
     return id;
   }
 
-  void setId(String id) {
-    id = id;
+  void setId(String id1) {
+    id = id1;
   }
 
   String getTitle() {
     return title;
   }
 
-  void setTitle(String title) {
-    title = title;
+  void setTitle(String title1) {
+    title = title1;
   }
 
   String getCreationDate() {
     return creationDate;
   }
 
-  void setCreationDate(String creationDate) {
-    creationDate = creationDate;
+  void setCreationDate(String creationDate1) {
+    creationDate = creationDate1;
   }
 
   String getLastModificationDate() {
     return lastModificationDate;
   }
 
-  void setLastModificationDate(String lastModificationDate) {
-    lastModificationDate = lastModificationDate;
+  void setLastModificationDate(String lastModificationDate1) {
+    lastModificationDate = lastModificationDate1;
   }
 
   String getExpirationDate() {
     return expirationDate;
   }
 
-  void setExpirationDate(String expirationDate) {
-    expirationDate = expirationDate;
+  void setExpirationDate(String expirationDate1) {
+    expirationDate = expirationDate1;
   }
 
   String getArrayPromemoria() {
     return arrayPromemoria;
   }
 
-  void setArrayPromemoria(String arrayPromemoria) {
-    arrayPromemoria = arrayPromemoria;
+  void setArrayPromemoria(String arrayPromemoria1) {
+    arrayPromemoria = arrayPromemoria1;
   }
 
   String getDescription() {
     return description;
   }
 
-  void setDescription(String description) {
-    description = description;
+  void setDescription(String description1) {
+    description = description1;
   }
 
-  Priority getPriority() {
+  String getPriority() {
     return priority;
   }
 
-  void setPriority(Priority priority) {
-    priority = priority;
+  void setPriority(Priority priority1) {
+    priority = priority1.toString();
   }
 
+  String getColor() {
+    return color;
+  }
 
+  void setColor(Color color1) {
+    color = color1.toString();
+  }
+
+  static Promemoria fromJson(Map<String, dynamic> data) {
+    Promemoria promemoria = Promemoria(
+        data['id'],
+        data['title'],
+        data['creationDate'],
+        data['lastModificationDate'],
+        data['expirationDate'],
+        data['arrayPromemoria'],
+        data['description'],
+        data['priority'],
+        data['color']);
+
+    print(promemoria.getId().toString());
+
+    return promemoria;
+  }
 }
