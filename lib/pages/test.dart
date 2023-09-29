5b02be9ae759d7dcd7272f5a30b56cbee011a79f
grata <aramis.grata@bluewin.ch>
2023-09-29 14:26:09 +0200

    removed controller

--- lib/pages/test.dart
+++ lib/pages/test.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:progetto_m335_flutter/database/controller.dart';
 import 'package:progetto_m335_flutter/database/database.dart';
 import 'package:progetto_m335_flutter/model/note.dart';
 import 'package:progetto_m335_flutter/model/promemoria.dart';
@@ -15,7 +14,6 @@ class Test extends StatefulWidget {
 
 class _TestState extends State<Test> {
   NoteDatabase noteDatabase = NoteDatabase.instance;
-  Controller controller = Controller();
 
   Future<void> _pressed() async {
     print("Inserting demo data");
@@ -44,14 +42,14 @@ class _TestState extends State<Test> {
         "alta",
         "rosso");
 
-    controller.addNote(nota);
-    controller.addPromemoria(promemoria);
+    noteDatabase.addNote(nota);
+    noteDatabase.addPromemoria(promemoria);
 
     final db = await noteDatabase.database;
 
     print("Printing data");
-    print((await controller.getAllPromemoria()).first);
-    print((await controller.getAllNote()).first);
+    print((await noteDatabase.getAllPromemoria()).first);
+    print((await noteDatabase.getAllNote()).first);
     print("promemoria");
     print(await db.query(promemoriaTable));
     print("note");
@@ -59,7 +57,7 @@ class _TestState extends State<Test> {
     print("Data printed");
 
     print("savedata from database to firebase");
-    controller.syncData();
+    noteDatabase.syncData();
     print("data saved");
 
   }
