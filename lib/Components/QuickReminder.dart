0628fcbbdba4ceb2373526b4ce5696dd03a301e4
Tito Arrigo <titoarrigo@gmail.com>
2023-09-29 14:44:12 +0200

    bo non lo so

--- lib/Components/QuickReminder.dart
+++ lib/Components/QuickReminder.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import '../database/controller.dart';
+import '../model/promemoria.dart';
+import '../navigation.dart';
 
 class QuickReminder extends StatefulWidget {
   const QuickReminder({super.key});
@@ -8,18 +11,34 @@ class QuickReminder extends StatefulWidget {
 }
 
 class _QuickReminderState extends State<QuickReminder> {
+  Controller controller = Controller();
+
   @override
   Widget build(BuildContext context) {
-    return const ListTile(
-      leading: Checkbox(
+    return ListTile(
+      leading: const Checkbox(
         value: false,
         onChanged: null,
       ),
       title: TextField(
-        decoration: InputDecoration(
-          labelText: 'New Reminder',
-        ),
-      ),
+          decoration: const InputDecoration(
+            labelText: 'New Reminder',
+          ),
+          onSubmitted: (String value) {
+            controller.addPromemoria(Promemoria.today(
+                value,
+                DateTime.now().toString(),
+                DateTime.now().toString(),
+                DateTime.now().toString(),
+                "description"));
+            Navigator.pushReplacement(
+              context,
+              PageRouteBuilder(pageBuilder: (context, animation1, animation2) {
+                return Navigation();
+              },
+            transitionDuration: const Duration(seconds: 0)),
+            );
+          }),
     );
   }
 }
